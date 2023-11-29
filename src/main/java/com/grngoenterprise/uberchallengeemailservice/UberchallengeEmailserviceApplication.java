79b159df2799d3cffdd1e687956aed7eabdaf774
Railson Magalhaes <railson.sistemas@gmail.com>
2023-11-29 10:12:43 -0300

    Iniciando desafio

---
+++ src/main/java/com/grngoenterprise/uberchallengeemailservice/UberchallengeEmailserviceApplication.java
@@ -0,0 +1,13 @@
+package com.grngoenterprise.uberchallengeemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberchallengeEmailserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberchallengeEmailserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grngoenterprise/uberchallengeemailservice/UberchallengeEmailserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grngoenterprise.uberchallengeemailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberchallengeEmailserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
